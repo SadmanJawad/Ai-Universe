f3592171e42ad2ab6599d11f1ac5703a6339efa7
SadmanJawad <jawadnabil977@gmail.com>
2023-03-03 13:04:49 +0600

    display card added dynamically in universe.js and  margin-y in the parent div

--- universe.js
+++ universe.js
@@ -1,12 +1,33 @@
 const loadTools = async() => {
-    const URL = `https://openapi.programming-hero.com/api/ai/tools`
-    const res = await fetch(URL);
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
     const data = await res.json();
+    displayTools(data.data.tools);
 
-    displayTools(data)
 }
 
 const displayTools = tools => {
-    console.log(tools)
+    // console.log(tools)
+    const toolsContainer = document.getElementById('tools-container');
+     tools.forEach(tool => {
+         const toolDiv = document.createElement('div');
+         toolDiv.classList.add('col');
+        toolDiv.innerHTML = `
+        <div class="card my-3">
+        <img src="${tool.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Feature</h5>
+          <p class="card-text">1. ${tool.features[0]}</p>
+          <p class="card-text">2. ${tool.features[1]}</p>
+          <p class="card-text">3. ${tool.features[2]}</p>
+        </div>
+      </div>
+        `;
+        console.log(tool)
+        toolsContainer.appendChild(toolDiv);
+
+
+     })
 }
+
 loadTools();
