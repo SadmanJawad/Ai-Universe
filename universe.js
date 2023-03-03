b4e8aa81b634eee1aea6d10e4658e04f5332b9ce
SadmanJawad <jawadnabil977@gmail.com>
2023-03-03 10:33:28 +0600

    load all-tools data in js file & add a initial card in html

--- universe.js
+++ universe.js
@@ -1,1 +1,12 @@
-console.log('connected');
+const loadTools = async() => {
+    const URL = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(URL);
+    const data = await res.json();
+
+    displayTools(data)
+}
+
+const displayTools = tools => {
+    console.log(tools)
+}
+loadTools();
