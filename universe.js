f33043f4245cf730d47b8bd21ea65357d3ed0ae7
SadmanJawad <jawadnabil977@gmail.com>
2023-03-04 00:04:06 +0600

    loader fixed

--- universe.js
+++ universe.js
@@ -1,28 +1,28 @@
-const loadTools = async () => {
+
+const loadTools = async (dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    displayTools(data.data.tools);
+    displayTools(data.data.tools, dataLimit);
 
 }
 
-const displayTools = tools => {
+const displayTools = (tools, dataLimit) => {
     // console.log(tools)
     const toolsContainer = document.getElementById('tools-container');
+    toolsContainer.innerHTML = '';
+
     // display 6  tool card only
     const showAll = document.getElementById('show-all');
-    if(tools.length > 6){
-        tools = tools.slice(0,6);
-        showAll.classList.remove('d-none');
+    if (dataLimit && tools.length > 6) {
+        tools = tools.slice(0, 6);
+        showAll.classList.remove('hidden');
     }
-    else{
-        showAll.classList.add('d-none')
+    else {
+        showAll.classList.add('hidden');
     }
-    console.log(showAll);
 
-
-
-
+    // display data
     tools.forEach(tool => {
         const toolDiv = document.createElement('div');
         toolDiv.classList.add('col');
@@ -30,7 +30,7 @@ const displayTools = tools => {
         <div class="card my-3">
         <img src="${tool.image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Feature</h5>
+          <h5 class="card-title font-bold">Feature</h5>
           <p class="card-text">1. ${tool.features[0]}</p>
           <p class="card-text">2. ${tool.features[1]}</p>
           <p class="card-text">3. ${tool.features[2]}</p>
@@ -39,35 +39,47 @@ const displayTools = tools => {
         <h4>${tool.name}</h4>
         <div class="d-flex justify-content-between px-2">
         <p><i class="fa-regular fa-calendar"></i> ${tool.published_in}</p>
+
         <button class="btn btn-outline-danger border-0"><i class="fa-solid fa-arrow-right"></i></button>
         </div>
       </div>
         `;
         // console.log(tool)
         toolsContainer.appendChild(toolDiv);
-    })
+    });
+    
+    // spinner or loader
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('hidden');
+    }
+    else {
+        loaderSection.classList.add('hidden');
+    }
+}
+
+    // Add event listener to Sort by Date button (bonus)
+    //    
+    // 
+    // 
+
     // stop loader / spinner
     toggleSpinner(false)
 }
 
-// handle show more button click
-const showAllButton = document.getElementById('btn-show-all').addEventListener('click', function(){
-// start loader / spinner
-toggleSpinner(true);
-
-    loadTools();
-
-})
-const toggleSpinner = isLoading => {
-    const loaderSection = document.getElementById('loader');
-    if(isLoading){
-        loaderSection.classList.remove('d-none');
-    }
-    else{
-        loaderSection.classList.add('d-none')
-    }
-
+// shortcut function
+const processSearch = (dataLimit) => {
+    loadTools(dataLimit);
+    toggleSpinner(true);
 }
 
+// handle see more button click
+document.getElementById('btn-show-all').addEventListener('click', function () {
+    processSearch();
+    // start loader / spinner
+    toggleSpinner(true);
+})
 
-loadTools();
+
+processSearch(6);
